d0f5522f7ccf92d8d3d590ec50187e58007914a3
Tristan914684 <tristanlyn44@gmail.com>
2026-08-19 00:18:47 +0800

    Add support for echo

--- src/main/java/Clsl.java
+++ src/main/java/Clsl.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Clsl {
     public static void main(String[] args) {
         String banner = "  ____ _     _ \n"
@@ -10,6 +12,14 @@ public class Clsl {
         String end = "Bye. Hope to see you again soon!";
         System.out.println(banner);
         System.out.println(greet);
+
+        Scanner scanner = new Scanner(System.in);
+        String userInput = scanner.nextLine();
+
+        while(!userInput.equals("bye")) {
+            System.out.println("\n" + userInput);
+            userInput = scanner.nextLine();
+        }
         System.out.println(end);
     }
 }
