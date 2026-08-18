6a85c12885bc10402b8b837079a770431c0a7f71
Tristan914684 <tristanlyn44@gmail.com>
2026-08-18 21:21:57 +0800

    Update name of ChatBot

---
+++ src/main/java/Clsl.java
@@ -0,0 +1,10 @@
+public class Clsl {
+    public static void main(String[] args) {
+        String banner = "  ____ _     _ \n"
+                + " / ___| |___| |\n"
+                + "| |   | / __| |\n"
+                + "| |___| \\__ \\ |\n"
+                + " \\____|_|___/_|\n";
+        System.out.println(banner);
+    }
+}
